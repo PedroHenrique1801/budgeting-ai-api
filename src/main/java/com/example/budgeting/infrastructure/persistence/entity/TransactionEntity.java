6886447f4d2d0cba93d5982bfa4276d9856a31b0
Pedro Henrique <pedrohsantos1801@gmail.com>
2026-06-24 11:08:28 -0300

    feat: commit inicial do projeto base de orçamentos com Spring AI

---
+++ src/main/java/com/example/budgeting/infrastructure/persistence/entity/TransactionEntity.java
@@ -0,0 +1,49 @@
+package com.example.budgeting.infrastructure.persistence.entity;
+
+
+import com.example.budgeting.domain.Category;
+import com.example.budgeting.domain.Transaction;
+import com.example.budgeting.domain.TransactionId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionEntity {
+    @Id
+    private UUID id;
+
+    private String description;
+    private long amount;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+
+
+    public static TransactionEntity from (Transaction transaction) {
+        return new TransactionEntity(
+                transaction.getId().uuid(),
+                transaction.getDescription(),
+                transaction.getAmount(),
+                transaction.getCategory());
+    }
+
+    public Transaction toDomain() {
+        return new Transaction(
+                new TransactionId(this.id),
+                this.description,
+                this.amount,
+                this.category
+        );
+    }
+}
